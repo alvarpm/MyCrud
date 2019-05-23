003e6c68aa8f696a9964833b87b0fbb356d6c593
Daniel G Perico Sánchez <dgpericos@gmail.com>
2019-05-23 11:43:41 -0500

    Se eliminó formulario de prueba

--- FormaPrueba.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class FormaPrueba
-    Private Sub FormaPrueba_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        MsgBox("Esta es una forma de prueba para compartir proyectos")
-    End Sub
-End Class
