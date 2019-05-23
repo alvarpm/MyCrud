aa537ee92cd368231291d4c721b0af1077ce734b
alvarpm <alvarpm@gmail.com>
2019-05-23 11:40:23 -0500

    Nueva forma modificada

--- FormaPrueba.vb
+++ FormaPrueba.vb
@@ -1,3 +1,5 @@
 ﻿Public Class FormaPrueba
-
+    Private Sub FormaPrueba_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Esta es una forma de prueba para compartir proyectos")
+    End Sub
 End Class
